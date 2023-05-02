0c4887671bdfa86c8da7e4abad85592e2b052275
梦清 <akaririn1028@gmail.com>
2023-05-02 20:28:56 +0800

    Create deploy-docker.sh

---
+++ deploy-docker.sh
@@ -0,0 +1,20 @@
+# docker engine on ubuntu安装脚本
+# 来自https://docs.docker.com/engine/install/ubuntu/
+# Akari Network
+
+apt-get remove docker docker-engine docker.io containerd runc
+apt update -y
+apt upgrade -y
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
