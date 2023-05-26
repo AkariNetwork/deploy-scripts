773c8d23c883ac32203de0f048a074e184b93834
梦清 <akaririn1028@gmail.com>
2023-05-26 23:23:00 +0800

    Create upgrade-portainer.sh

---
+++ upgrade-portainer.sh
@@ -0,0 +1,8 @@
+# portainer on docker standalone更新脚本
+# 来自https://docs.portainer.io/start/upgrade/docker
+# Akari Network
+
+docker stop portainer
+docker rm portainer
+docker pull portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
